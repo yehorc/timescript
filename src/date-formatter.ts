9a213b7743b1b06faec9efda711d925f6e7b1ec3
Yehor Chornyi <139161208+yehorc@users.noreply.github.com>
2024-04-19 11:44:44 +0300

    Feat lint (#2)
    * Added eslint packages.
    
    * Added a lint job.
    
    * Update ci.yml
    
    * Fix.
    
    * Fix.
    
    * Fix.

--- src/date-formatter.ts
+++ src/date-formatter.ts
@@ -1,29 +1,45 @@
+// ./src/date-formatter.ts
+
 /* List of all available parsing tokens
 -----------------------------------------
 Input   Example             Description
 -----------------------------------------
 YY	    01	                Two-digit year
-YYYY	2001	            Four-digit year
-MM	    01-12	            Month, 2-digits
+YYYY	  2001	              Four-digit year
+MM	    01-12	              Month, 2-digits
 MMM	    Jan-Dec	            The abbreviated month name
-D	    1-31	            Day of month
-DD	    01-31	            Day of month, 2-digits
+D	      1-31	              Day of month
+DD	    01-31	              Day of month, 2-digits
 */
 
-export function formatDate(format: string, date: Date): string {
-    const tokenMap: {[key: string]: string} = {
-        'YYYY': date.getFullYear().toString(),
-        'YY': date.getFullYear().toString().slice(-2),
-        'MMM': date.toLocaleString('default', { month: 'short' }),
-        'MM': (date.getMonth() + 1).toString().padStart(2, '0'),
-        'DD': date.getDate().toString().padStart(2, '0'),
-        'D': date.getDate().toString(),
-    };
+export function formatDate (format: string, date: Date = new Date()): string {
+  if (!/^(YY|YYYY|MMM|MM|DD|D)(-(YY|YYYY|MMM|MM|DD|D))*$/.test(format)) {
+    throw new Error("Invalid format string. Use YY, YYYY, MMM, MM, DD, D seperated by '-'")
+  }
 
-    let formattedDate = format;
-    for (const token in tokenMap) {
-        formattedDate = formattedDate.replace(token, tokenMap[token]);
+  // pad single digit numbers with a leading zero
+  const pad = (n: number) => n < 10 ? `0${n}` : `${n}`
+
+  // month names
+  const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+  // Replace format string with actual date parts
+  return format.replace(/YYYY|YY|MMM|MM|DD|D/g, (match) => {
+    switch (match) {
+      case 'YY':
+        return date.getFullYear().toString().slice(-2)
+      case 'YYYY':
+        return date.getFullYear().toString()
+      case 'MMM':
+        return monthNames[date.getMonth()]
+      case 'MM':
+        return pad(date.getMonth() + 1)
+      case 'DD':
+        return pad(date.getDate())
+      case 'D':
+        return date.getDate().toString()
+      default:
+        return match
     }
-
-    return formattedDate;
+  })
 }
